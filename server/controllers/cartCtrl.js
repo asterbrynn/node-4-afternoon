4552faf215bb48f48becd74dcd9296c74c3412de
Aster Brynn <aster.gibby@gmail.com>
2019-06-15 20:28:00 -0600

    step 10

--- server/controllers/cartCtrl.js
+++ server/controllers/cartCtrl.js
@@ -4,12 +4,23 @@ module.exports = {
 	add: (req, res) => {
 		const {id} = req.params;
 		let {user} = req.session;
-		if (!user.cart.find(id)) {
+		if (user.cart.find(swag => swag.id == id)) {
+			res.status(200).send(user);
+		}
+		else {
+			let item = swag.find(swag => swag.id == id);
+			user.cart.push(item);
+			user.total += item.price;
 			res.status(200).send(user);
 		}
 	},
 	delete: (req, res) => {
 		const {id} = req.params;
+		let {user} = req.session;
+		let index = user.cart.findIndex(swag => swag.id == id);
+		user.cart.splice(index, 1);
+		user.total -= user.cart[index].price;
+		res.status(200).send(user);
 	},
 	checkout: (req, res) => {
 		const {user} = req.session;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 require('dotenv').config();
 const swagCtrl = require('./controllers/swagCtrl');
 const authCtrl = require('./controllers/authCtrl');
+const cartCtrl = require('./controllers/cartCtrl');
 
 const checkForSession = require('./middlewares/checkForSession');
 let {SERVER_PORT, SESSION_SECRET} = process.env;
@@ -22,5 +23,9 @@ app.post('/api/login', authCtrl.login);
 app.post('/api/register', authCtrl.register);
 app.post('/api/signout', authCtrl.signout);
 app.get('/api/user', authCtrl.getUser);
+
+app.post('/api/cart/checkout', cartCtrl.checkout);
+app.post('/api/cart/:id', cartCtrl.add);
+app.delete('/api/cart/:id', cartCtrl.delete);
 
 app.listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`));
